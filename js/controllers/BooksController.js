6ac585897728e75d48680444b6c78ea125385411
Oddgeir Gitlestad <oddgeir.gitlestad@gitlestadit.no>
2013-08-16 00:42:20 +0200

    Added glyphicons. Added a delete book function. Added ISBN validator.

--- js/controllers/BooksController.js
+++ js/controllers/BooksController.js
@@ -9,4 +9,49 @@ function BooksController($scope, angularFireCollection) {
     var url = 'https://gitlebook.firebaseio.com/books/';
     $scope.books = angularFireCollection(url);
 
+    $scope.registerBook = function(book){
+
+        if(isValidISBN(book['isbn'])){
+            $scope.books.add(book);
+            alert("Book added!");
+            document.getElementById('registerBookForm').reset();
+        }
+        else {
+            alert(book['isbn'] + " is not a valid ISBN!");
+        }
+
+    }
+    $scope.delBook = function(bookIndex){
+        alert("Are you sure?");
+        $scope.books.remove($scope.books[bookIndex]);
+    }
+
+}
+function isValidISBN(isbn){
+    isbn = isbn.replace(/\-/g,'');
+    isbn = isbn.replace(/\+/g,'');
+    alert(isbn);
+    var digits = isbn.split('');
+    var sum = 0;
+
+    if (isbn.length == 13 && /^\d+$/.test(isbn) ){
+        for (i = 0; i < 13; i += 2){
+            sum += +parseInt(digits[i]);
+        }
+        for (i = 1; i < 12; i += 2){
+            sum += (3 * +parseInt(digits[i]));
+        }
+        return sum % 10 === 0;
+    }
+    else if (isbn.length == 10){
+        for (i = 0; i < digits.length; i++){
+            sum += ((10-i) * parseInt(digits[i]));
+        }
+        alert(sum + 'test');
+        return ((sum % 11) == 0);
+
+    }else
+
+//    return ((sum % 11) == 0);
+    return false;
 }
