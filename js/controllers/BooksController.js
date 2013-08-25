dead31c572ac24468e73bf47f8d612a86c88bacd
Oddgeir Gitlestad <oddgeir.gitlestad@gitlestadit.no>
2013-08-25 21:38:45 +0200

    Fixed the edit book function. Added borrowing function

--- js/controllers/BooksController.js
+++ js/controllers/BooksController.js
@@ -12,6 +12,10 @@ function BooksController($scope, angularFireCollection) {
     $scope.registerBook = function(book){
 
         if(isValidISBN(book['isbn'])){
+            book.loaners = [];
+            var test = {Name: 'Oddgeir'};
+            book.loaners.push(test);
+
             $scope.books.add(book);
             $('#bookAddedModal').modal('show');
             document.getElementById('registerBookForm').reset();
@@ -20,20 +24,55 @@ function BooksController($scope, angularFireCollection) {
             alert(book['isbn'] + " is not a valid ISBN!");
         }
 
-    }
+    };
     $scope.delBook = function(bookIndex){
         if (confirm("Are you sure?")){
             $scope.books.remove($scope.books[bookIndex]);
         }
 
-    }
+    };
 
-    $scope.editBook = function(index){
-        if (confirm("Are you sure?")){
+    $scope.editBookFn = function(book, index){
+
+        $scope.books[index].title = book.title;
+        $scope.books[index].author = book.author;
+        $scope.books[index].genre = book.genre;
+        $scope.books[index].rating = book.rating;
+        $scope.books[index].price = book.price;
+
+    };
+    $scope.editBookModal = function(index){
+        $scope.index = index;
+        $scope.editBook = {};
+        $scope.editBook.title = $scope.books[index].title;
+        $scope.editBook.author = $scope.books[index].author;
+        $scope.editBook.genre = $scope.books[index].genre;
+        $scope.editBook.rating = $scope.books[index].rating;
+        $scope.editBook.price = $scope.books[index].price;
+
+        $('#editBookModal').modal("show");
+    };
+
+    $scope.borrowBook = function(name, index){
+        var loaner = {Name: name};
+        var book = $scope.books[index];
+        if (book.loaners == null){
+            book.loaners = [];
+            book.loaners.push(loaner);
             $scope.books.update(book);
+            alert('First Book loaned');
+        }
+        else {
+            book.loaners.push(loaner);
+            $scope.books.update(book);
+            alert('Book loaned');
         }
 
+    };
+    $scope.borrowBookModal= function(index){
+        $scope.index = index;
+        $('#borrowBookModal').modal("show");
+    };
 
 
-}
 }
